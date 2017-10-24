d79a5f86c97843a357cef41f5e41e11055aaf82f
thambt <thambui2503@gmail.com>
2017-10-24 22:08:50 +0700

    nothign

--- api/controller/socketController.js
+++ api/controller/socketController.js
@@ -28,7 +28,7 @@ module.exports = function (io) {
         })
         socket.on("cBuy", function(arrProduct, userCustomer) {
             console.log(arrProduct)
-             Array.from(arrProduct).forEach(function(element) {
+            arrProduct.forEach(function(element) {
                   console.log(element)
                /* product.findById(element._id,function(result, err){
                     if(!result) {
