e24f4dd3c2ce261c3a1d1f20b8557b66481acc62
thambt <thambui2503@gmail.com>
2017-10-24 15:15:22 +0700

    Socket IO

--- api/controller/socketController.js
+++ api/controller/socketController.js
@@ -1,9 +1,38 @@
-/*module.exports = function (io) {
+var user = require("../model/user");
+var bill = require("../model/bill")
+var product = require("../model/product")
+
+
+module.exports = function (io) {
     io.sockets.on('connection', function (socket) {
         console.log("co nguoi ket noi");
+
+        socket.on('email', function (data) {
+            socket.id = data;
+            socketID.forEach(function (element) {
+                if (socket.id === element) {
+                    check = 1;
+                }
+            }, this);
+            if (check === 0) {
+                socketID.push(socket.id);
+            }
+            else {
+                check = 0;
+            }
+        });
 
         socket.on('disconnect', function () {
             console.log(socket.id + " disconnect");
         })
+        socket.on("cBuy", function(arrProduct, userCustomer) {
+            arrProduct.forEach(function(element) {
+                product.findById(element._id,function(result, err){
+                    if(!result) {
+                        socket.broadcast.emit("SopNewBill", {"userCustomer" : userCustomer, "isProduct" : element._id,"idShop": result._id})
+                    }
+                })
+            })
+        })
     })
-}*/
+}
