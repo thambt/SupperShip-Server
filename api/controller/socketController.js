9620ad54e3f91297cbaf79bbade0fbe0f09c4b9e
thambt <thambui2503@gmail.com>
2017-10-24 21:59:51 +0700

    no message

--- api/controller/socketController.js
+++ api/controller/socketController.js
@@ -27,14 +27,14 @@ module.exports = function (io) {
             console.log(socket.id + " disconnect");
         })
         socket.on("cBuy", function(arrProduct, userCustomer) {
-            console.log(arrProduct[0])
-            /* arrProduct.forEach(function(element) {
-                product.findById(element._id,function(result, err){
+            console.log(arrProduct)
+             Array.from(arrProduct).forEach(function(element) {
+               /* product.findById(element._id,function(result, err){
                     if(!result) {
                         socket.broadcast.emit("SopNewBill", {"userCustomer" : userCustomer, "isProduct" : element._id,"idShop": result._id})
                     }
-                })
-            }) */
+                })*/
+            }) 
         })
     })
 }
