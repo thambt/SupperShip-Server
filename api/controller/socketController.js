643089986cddbf09972105981b22529465781de1
thambt <thambui2503@gmail.com>
2017-10-25 11:22:46 +0700

    no message

--- api/controller/socketController.js
+++ api/controller/socketController.js
@@ -27,9 +27,8 @@ module.exports = function (io) {
             console.log(socket.id + " disconnect");
         })
         socket.on("cBuy", function(arrProduct, userCustomer) {
-            var myJSON = JSON.stringify(arrProduct);
-            console.log(myJSON)
-            myJSON.forEach(function(element) {
+            console.log(arrProduct)
+            Array.from(arrProduct).forEach(function(element) {
                   console.log(element)
                /* product.findById(element._id,function(result, err){
                     if(!result) {
