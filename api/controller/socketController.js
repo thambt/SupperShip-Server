e1c2ba726e9bf41f36f7963e14f8ec8cbe8f942c
thambt <thambui2503@gmail.com>
2017-10-25 11:35:43 +0700

    emit when custom buy a product

--- api/controller/socketController.js
+++ api/controller/socketController.js
@@ -30,11 +30,11 @@ module.exports = function (io) {
             console.log(arrProduct)
             Array.from(arrProduct).forEach(function(element) {
                   console.log(element)
-               /* product.findById(element._id,function(result, err){
+               product.findById(element._id,function(result, err){
                     if(!result) {
-                        socket.broadcast.emit("SopNewBill", {"userCustomer" : userCustomer, "isProduct" : element._id,"idShop": result._id})
+                        socket.broadcast.emit("SopNewBill", {"userCustomer" : userCustomer, "idProduct" : element._id,"emailShop": element.emailShop, "quantity": element.quantity})
                     }
-                })*/
+                })
             }) 
         })
     })
