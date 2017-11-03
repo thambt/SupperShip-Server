ef2a494967e28fb2d9a3ca6488e81c59dea43c5c
thambt <thambui2503@gmail.com>
2017-11-03 20:54:40 +0700

    Fixed API get product for shop by kind

--- api/controller/bussinessController.js
+++ api/controller/bussinessController.js
@@ -89,7 +89,7 @@ module.exports = function (app, passport, io) {
 
     // get Product theo loai cho shop
     app.get("/shop/shopFilterProduct/:kind/:email", function (req, res) {
-         product.find({email : req.params.email, kind: { $regex: "^" + req.params.kind }  },function (err, result) {
+         product.find({emailShop : req.params.email, kind: { $regex: "^" + req.params.kind }  },function (err, result) {
             res.json({ "arrProduct": result });
         })
     })
