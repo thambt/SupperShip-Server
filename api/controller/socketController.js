37c981b2c1ebb2abd819cd6c14949392d4cc8820
thambt <thambui2503@gmail.com>
2017-11-11 13:34:38 +0700

    Add shipper to room;

--- api/controller/socketController.js
+++ api/controller/socketController.js
@@ -9,7 +9,7 @@ module.exports = function (io) {
     io.sockets.on('connection', function (socket) {
         console.log("co nguoi ket noi");
 
-        socket.on('email', function (data) {
+        socket.on('email', function (data, kindUser) {
             socket.id = data;
             socketID.forEach(function (element) {
                 if (socket.id === element) {
@@ -18,6 +18,7 @@ module.exports = function (io) {
             }, this);
             if (check === 0) {
                 socketID.push(socket.id);
+                socket.join('roomShipper');
             }
             else {
                 check = 0;
@@ -28,68 +29,78 @@ module.exports = function (io) {
             console.log(socket.id + " disconnect");
         })
 
-         socket.on("shopAcceptBill", function (code,idBill) {
-             console.log(idBill)
-             bill.findById(idBill, function(err, data)
-             {
-                 if(err==null){
-                     if(code== 1)
-                     console.log(data)
-                     if(data != null)
-                     {
-                         client.broadcast.to(arrayOfIds)
-                         client.broadcast.to("custom@gmail.com").emit("shopAcceptYourBill",{"code": code})
-                        // socket.emit("shopAcceptYourBill",code)
-                 }
-                     // socket.broadcast.emit("shopAcceptYourBill",data.emailCustomer, idBill,code)
-                 }
-             })
-         })
+        // gửi sự kiện cho khách khi shop accept đơn hàng
+        socket.on("shopAcceptBill", function (code, idBill) {
+            console.log(idBill)
+            bill.findById(idBill, function (err, data) {
+                    if (data != null) {
+                         bill.update({ _id: idBill}, { $set: { "status": code } }, function (err, result) {
+                             if(result != null)
+                             {
+                                 client.broadcast.to("custom@gmail.com").emit("shopAcceptYourBill", { "code": code })
+                             }
+                         })
+                    }
+            })
+        })
 
+
+        // gửi broadcast cho shipper khi có bill mới:
+        socket.on("haveNewBill", function ( idBill) {
+          //  console.log(idBill)
+            bill.findById(idBill, function (err, data) {
+                    if (data != null) {
+                        // io.sockets.in('room')
+                        io.sockets.in('roomShipper').emit("haveNewBillShip", { "code": code })
+                        // socket.emit("shopAcceptYourBill",code)
+                    }
+                    // socket.broadcast.emit("shopAcceptYourBill",data.emailCustomer, idBill,code)
+            })
+        })
 
         socket.on("cBuy", function (arrProduct, userCustomer, time) {
             console.log(time)
             Array.from(arrProduct).forEach(function (element) {
-               user.findOne({ email: element.email }, function (err, result) {
-                    console.log("find a user",result)
-                    if(err == null){
-                    var newBill = {
-                        emailShop: element.email,
-                        emailCustomer: userCustomer.email,
-                        phoneSend: result.phone,
-                        phoneReceive: userCustomer.phone,
-                        phoneShipper: '',
-                        addressReceive: userCustomer.address,
-                        addressSend: result.addressShop,
-                        status: 0,
-                        listProduct: element.listProduct,
-                        moneyItem: 300000,
-                        moneyShip: 20000,
-                        time: '',
-                        note: "String"
-                    }
-                    bill.create(newBill, function (err, result) {
-                        if (!err) {
-                            console.log("create Bill",result)
-                            var newNoti = {
-                                emailShop: result.emailShop,
-                                nameActor: userCustomer.email,
-                                action: "mua hang",
-                                idBill: result._id,
-                                read: false,
-                                status: 0,
-                                time: time
-                            };
-                             user.findOneAndUpdate({ email: result.emailShop }, { $push: { listNoti: newNoti } }, { safe: true, upsert: true, new: true },
-                                    function (err, data) {
-                                console.log("create Noti",data)
-                            })
-                                   socket.broadcast.emit("SopNewBill", { "emailCustom": userCustomer.email, "phoneCustomer": userCustomer.phone, "emailShop":element.email, "idBill": result._id, "action": " mua hang " })
+                user.findOne({ email: element.email }, function (err, result) {
+                    console.log("find a user", result)
+                    if (err == null) {
+                        var newBill = {
+                            emailShop: element.email,
+                            emailCustomer: userCustomer.email,
+                            phoneSend: result.phone,
+                            phoneReceive: userCustomer.phone,
+                            phoneShipper: '',
+                            addressReceive: userCustomer.address,
+                            addressSend: result.addressShop,
+                            status: 0,
+                            listProduct: element.listProduct,
+                            moneyItem: 300000,
+                            moneyShip: 20000,
+                            time: '',
+                            note: "String"
                         }
-                    })
-               }
+                        bill.create(newBill, function (err, result) {
+                            if (!err) {
+                                console.log("create Bill", result)
+                                var newNoti = {
+                                    emailShop: result.emailShop,
+                                    nameActor: userCustomer.email,
+                                    action: "mua hang",
+                                    idBill: result._id,
+                                    read: false,
+                                    status: 0,
+                                    time: time
+                                };
+                                user.findOneAndUpdate({ email: result.emailShop }, { $push: { listNoti: newNoti } }, { safe: true, upsert: true, new: true },
+                                    function (err, data) {
+                                        console.log("create Noti", data)
+                                    })
+                                socket.broadcast.emit("SopNewBill", { "emailCustom": userCustomer.email, "phoneCustomer": userCustomer.phone, "emailShop": element.email, "idBill": result._id, "action": " mua hang " })
+                            }
+                        })
+                    }
                 })
-               
+
             })
         })
     })
