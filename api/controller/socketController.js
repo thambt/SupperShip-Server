5d05fd6fbc7a7ec1be41ac322c97fcc42f42350a
thambt <thambui2503@gmail.com>
2017-11-28 00:07:49 +0700

    fix bug in socket

--- api/controller/socketController.js
+++ api/controller/socketController.js
@@ -32,7 +32,7 @@ module.exports = function (io) {
 
         // gửi sự kiện cho khách khi shop accept/deny đơn hàng
         socket.on("shopAcceptBill", function (emailShop, emailCustomer, code, idBill, time) {
-          //  console.log('shopAcceptBill')
+            //  console.log('shopAcceptBill')
             bill.findById(idBill, function (err, data) {
                 if (data != null) {
 
@@ -48,7 +48,7 @@ module.exports = function (io) {
                     };
                     user.findOneAndUpdate({ email: data.emailCustomer }, { $push: { listNoti: newNoti } }, { safe: true, upsert: true, new: true },
                         function (err, User) {
-                           // console.log("create Noti", User)
+                            // console.log("create Noti", User)
                         })
                     if (data != null) {
                         if (code == 1) {
@@ -99,7 +99,7 @@ module.exports = function (io) {
             //  console.log(idBill)
             bill.findById(idBill, function (err, data) {
                 if (data != null) {
-                    if (data.status == 2) {
+                    if (data.status == 1) {
                         var newNoti = {
                             nameActor: emailShipper,
                             myEmail: data.emailShop,
@@ -112,61 +112,86 @@ module.exports = function (io) {
                         };
                         user.findOneAndUpdate({ email: data.emailShop }, { $push: { listNoti: newNoti } }, { safe: true, upsert: true, new: true },
                             function (err, result) {
-                              //  console.log("create Noti", data)
+                                //  console.log("create Noti", data)
                             })
                         bill.findOneAndUpdate({ _id: idBill }, { $push: { listShipperRegister: emailShipper } }, { safe: true, upsert: true, new: true },
                             function (err, result) {
-                               // console.log("create Noti", data)
+                                // console.log("create Noti", data)
                             })
-                       // console.log("send ", idBill)
+                        // console.log("send ", idBill)
                         socket.broadcast.emit("haveShipperRegister", { "idBill": idBill, "emailShipper": emailShipper, "emaiShop": data.emailShop })
                     }
                     else {
-                        socket.broadcast.emit("shipperBillHaveShip", { "idBill": idBill, "status": 1 })// 1 la Bill da co ship roi
+                        var newNoti = {
+                            nameActor: data.emailShop,
+                            myEmail: emailShipper,
+                            action: 401,
+                            idBill: idBill,
+                            read: false,
+                            status: 0,
+                            time: time,
+                            method: data.methodTransform
+                        };
+                        user.findOneAndUpdate({ email: emailShipper }, { $push: { listNoti: newNoti } }, { safe: true, upsert: true, new: true },
+                            function (err, result) {
+                                //  console.log("create Noti", data)
+                            })
+                        socket.broadcast.emit("shipperNoti", { "myEmail": emailShipper })// 1 la Bill da co ship roi
                     }
                 }
                 else {
-                    socket.broadcast.emit("shipperBillHaveShip", { "idBill": idBill, "status": 0 })// 0 laf bill k con ton tai
+                    var newNoti = {
+                        nameActor: "",
+                        myEmail: emailShipper,
+                        action: 400,
+                        idBill: idBill,
+                        read: false,
+                        status: 0,
+                        time: time,
+                        method: data.methodTransform
+                    };
+                    user.findOneAndUpdate({ email: emailShipper }, { $push: { listNoti: newNoti } }, { safe: true, upsert: true, new: true },
+                        function (err, result) {
+                            //  console.log("create Noti", data)
+                        })
+                    socket.broadcast.emit("shipperNoti", { "myEmail": emailShipper })// 0 laf bill k con ton tai
                 }
             })
         })
 
 
         // shop accept shipper
-        socket.on("shopAcceptShipper", function (code, idBill, time, emailShipper) {
-           // console.log("shopAcceptShipper", emailShipper)
+        socket.on("shopAcceptShipper", function (code, idBill, time, emailShipper, emailShop) {
+            // console.log("shopAcceptShipper", emailShipper)
             bill.findById(idBill, function (err, data) {
                 if (data != null) {
                     user.findOne({ email: emailShipper }, function (err, userShipper) {
                         if (err == null) {
-                            var newNoti = {
-                                nameActor: data.emailShop,
-                                myEmail: emailShipper,
-                                action: code,
-                                idBill: idBill,
-                                read: false,
-                                status: 0,
-                                time: time,
-                                method: data.methodTransform
-                            };
-                            user.findOneAndUpdate({ email: emailShipper }, { $push: { listNoti: newNoti } }, { safe: true, upsert: true, new: true },
-                                function (err, data) {
-                                    //console.log("create Noti", data)
-                                })
                             if (code == 1) {
+                                var newNoti = {
+                                    nameActor: data.emailShop,
+                                    myEmail: emailShipper,
+                                    action: 200,
+                                    idBill: idBill,
+                                    read: false,
+                                    status: 0,
+                                    time: time,
+                                    method: data.methodTransform
+                                };
+                                user.findOneAndUpdate({ email: emailShipper }, { $push: { listNoti: newNoti } }, { safe: true, upsert: true, new: true },
+                                    function (err, data) {
+                                        //console.log("create Noti", data)
+                                    })
                                 bill.update({ _id: idBill }, { $set: { "status": 2, emailShipper: emailShipper, phoneShipper: userShipper.phone } }, function (err, result) {
                                     if (result != null) {
-                                        socket.broadcast.emit("shipperShopAcceptYou", { "idBill": idBill, "status": 1, "emailShop": data.emailShop, "emaiShipper": emailShipper })
-                                        console.log("create Noti", data)
+                                        socket.broadcast.emit("shipperShopAcceptYou", { "idBill": idBill, "status": 1, "emailShop": emailShop, "emailShipper": emailShipper })
                                     }
                                 })
                             }
-                        } else {
-                            socket.broadcast.emit("ShopResponeShopAcceptYou", { "idBill": idBill, "code": 0, "emailShop": newNoti.nameActor, emaiShipper: emailShipper })
-                        }
+                        } 
                     })
 
-                }  else {
+                } else {
                     socket.broadcast.emit("shopBillNotFound", { "idBill": idBill, "emailShop": emailShop })
                 }
             })
@@ -174,10 +199,10 @@ module.exports = function (io) {
         })
 
         socket.on("cBuy", function (arrProduct, userCustomer, time, methodTransform) {
-           // console.log(time)
+            // console.log(time)
             Array.from(arrProduct).forEach(function (element) {
                 user.findOne({ email: element.email }, function (err, result) {
-                   // console.log("find a user", result)
+                    // console.log("find a user", result)
                     if (err == null) {
                         var newBill = {
                             emailShop: element.email,
@@ -197,7 +222,7 @@ module.exports = function (io) {
                         }
                         bill.create(newBill, function (err, result) {
                             if (err == null) {
-                               // console.log("create Bill", result)
+                                // console.log("create Bill", result)
                                 var newNoti = {
                                     myEmail: result.emailShop,
                                     nameActor: userCustomer.email,
@@ -210,7 +235,7 @@ module.exports = function (io) {
                                 };
                                 user.findOneAndUpdate({ email: result.emailShop }, { $push: { listNoti: newNoti } }, { safe: true, upsert: true, new: true },
                                     function (err, data) {
-                                       // console.log("create Noti", data)
+                                        // console.log("create Noti", data)
                                     })
                                 socket.broadcast.emit("SopNewBill", { "emailCustom": userCustomer.email, "phoneCustomer": userCustomer.phone, "emailShop": element.email, "idBill": result._id, "action": " mua hang " })
                             }
